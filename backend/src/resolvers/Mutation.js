0834e48116f44b2ddc6f8dbb31a061ace7077b69
Dean <dean@deangaudet.com>
2018-10-27 19:28:05 -0600

    - backend/src/schema - adding deleteItem mutation - backend/src/Mutation - adding deleteItem mutation - frontend/components/DeleteItem - adding delete item component - frontend/components/Item - replace delete button with DeleteItem component

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -18,8 +18,12 @@ const Mutations = {
         id: args.id
       }
     },
-    info
-  );
+    info);
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
