40c770e2a3ce7e57c143cb4f23a1fa4fddb98d7d
Dean <dean@deangaudet.com>
2018-11-11 13:16:51 -0700

    - backend/datamodel - adding user as a field on the item - backend/src/generated/prisma - generated file changed after adding the user field - backend/src/resolvers/Mutation - throw an error if attempting to create an item with no user, also setting the user relationship on the item if the user exists - frontend/components/PleaseSignIn - adding component to ask the user to sign in on pages which require it - frontend/pages/sell - using PleaseSignIn component to ask the user to sign in if required when loading the page

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -6,8 +6,17 @@ const { transport, makeAniceEmail } = require('../mail');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
+    if(!ctx.request.userId) {
+      throw new Error('You must be logged in to do this');
+    }
     const item = await ctx.db.mutation.createItem({
       data: {
+        // this is the relation ship between the item and the user
+        user: {
+          connect: {
+            id: ctx.request.userId
+          }
+        },
         ...args
       }
     }, info);
