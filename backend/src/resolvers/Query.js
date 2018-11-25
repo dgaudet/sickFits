359b7602ca90850a1e641c8f866c6606f1c746db
Dean <dean@deangaudet.com>
2018-11-24 20:01:45 -0700

    - backend/datamodel - adding createdAt and updatedAt to Order so it can be returned - backend/src/generated/prisma - re-generating schema stuff - backend/src/schema - adding order Query - backend/src/resolvers/Query - adding order query logic - frontend/components/TakeMyMoney - sending user to the order page after order has completed - frontend/components/Order - adding component to show an order - frontend/pages/order - loading order component

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -20,6 +20,21 @@ const Query = {
     hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
 
     return ctx.db.query.users({}, info);
+  },
+  async order(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in.');
+    }
+    const order = await ctx.db.query.order({
+      where: { id: args.id },
+    }, info);
+
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToViewOrder = ctx.request.user.permissions.includes('ADMIN');
+    if(!ownsOrder && !hasPermissionToViewOrder) {
+      throw new Error('You cannot view this order.');
+    }
+    return order;
   }
 };
 
